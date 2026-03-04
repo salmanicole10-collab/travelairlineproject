5df7c2d5a28cbd5d812f70a09092444329edc042
salmanicole10-collab <salmanicole10@gmail.com>
2026-03-04 13:30:48 -0400

    fix

--- travelagent.py
+++ travelagent.py
@@ -6,9 +6,17 @@ def get_country_by_name(name):
 
     url = "https://restcountries.com/v3.1/name/" + name
     r = requests.get(url)
+    
+    try:
+        response = requests.get(url)
 
-    if r.status_code != 200:
+        if response.status_code == 200:
+            info = response.json()
+            return info[0]
+        else:
+            print("Error:", response.status_code)
+            return None
+
+    except Exception as e:
+        print("Error:", e)
         return None
-
-    data = r.json()
-    return data[0]
