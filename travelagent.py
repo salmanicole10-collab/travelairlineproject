007691e5eebf91d755eb0b75c227a4468ee36727
salmanicole10-collab <salmanicole10@gmail.com>
2026-03-09 15:53:19 -0400

    arreglos

--- travelagent.py
+++ travelagent.py
@@ -5,7 +5,6 @@ def get_country_by_name(name):
         return None
 
     url = "https://restcountries.com/v3.1/name/" + name
-    r = requests.get(url)
 
     try:
         response = requests.get(url)
@@ -20,61 +19,3 @@ def get_country_by_name(name):
     except Exception as e:
         print("Error:", e)
         return None
-
-def show_country(country):
-
-    print("--- COUNTRY INFO ---")
-
-    official = country["name"]["official"]
-    print("Official:", official)
-
-    if "capital" in country:
-        capital = country["capital"][0]
-    else:
-        capital = "N/A"
-    print("Capital:", capital)
-
-    if "region" in country:
-        region = country["region"]
-    else:
-        region = "N/A"
-    print("Region:", region)
-
-    if "subregion" in country:
-        subregion = country["subregion"]
-    else:
-        subregion = "N/A"
-    print("Subregion:", subregion)
-
-    if "population" in country:
-        population = country["population"]
-    else:
-        population = "N/A"
-    print("Population:", population)
-
-    if "currencies" in country:
-        currencies = country["currencies"]
-        print("Currency:", ", ".join(currencies.keys()))
-    else:
-        print("Currency: N/A")
-
-    if "languages" in country:
-        languages = country["languages"]
-        print("Languages:", ", ".join(languages.values()))
-    else:
-        print("Languages: N/A")
-
-    if "cca2" in country and "cca3" in country:
-        print("Code:", country["cca2"], "/", country["cca3"])
-    else:
-        print("Code: N/A")
-
-    if "timezones" in country:
-        print("Timezones:", ", ".join(country["timezones"]))
-    else:
-        print("Timezones: N/A")
-
-    if "flags" in country and "png" in country["flags"]:
-        print("Flag:", country["flags"]["png"])
-    else:
-        print("Flag: N/A")
